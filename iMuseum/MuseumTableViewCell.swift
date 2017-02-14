51fcd578e2fd9dc1ff5a502c92a81feaa0147386
Valerie Greer <vagreer10@Valeries-MacBook-Pro.local>
2017-02-14 01:41:26 -0500

    Downloads museum Json, parses museum Json, displays museum "common name", "location_1_address", "location_1_city", "location_1_state", "location_1_zip" in TableView.

---
+++ iMuseum/MuseumTableViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  MuseumTableViewCell.swift
+//  iMuseum
+//
+//  Created by Valerie Greer on 2/14/17.
+//  Copyright © 2017 Shane Empie. All rights reserved.
+//
+
+import UIKit
+
+class MuseumTableViewCell: UITableViewCell {
+
+    @IBOutlet var museumNameLabel       :UILabel!
+    @IBOutlet var museumAddressLabel    :UILabel!
+
+}
